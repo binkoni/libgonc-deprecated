ebea39f38e8cea5176564cad90e24b48381ec65e
Byeonggon Lee <gonny952@gmail.com>
2016-06-28 03:48:16 +0900

    gonc_array_list passed the test

--- src/test/gonc_array_list_test.c
+++ src/test/gonc_array_list_test.c
@@ -66,7 +66,6 @@ void prepend_and_remove_test()
     assert_ptr_not_equal(array_list, NULL);
 
     int* value;
-
     value = malloc(sizeof(int));
     *value = 100;
     assert_int_not_equal(gonc_array_list_prepend(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
@@ -87,64 +86,94 @@ void prepend_and_remove_test()
     *value = 500;
     assert_int_not_equal(gonc_array_list_prepend(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
 
-    struct gonc_entry* entry = gonc_array_list_remove(array_list, 0);    
+    struct gonc_entry* entry = gonc_array_list_remove(array_list, 0);   
+    assert_ptr_not_equal(entry, NULL);
     assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 500);
     gonc_entry_destroy(entry);
 
     entry = gonc_array_list_remove(array_list, 0);    
+    assert_ptr_not_equal(entry, NULL);
     assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 400);
     gonc_entry_destroy(entry);
 
     entry = gonc_array_list_remove(array_list, 0);    
+    assert_ptr_not_equal(entry, NULL);
     assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 300);
     gonc_entry_destroy(entry);
 
-    entry = gonc_array_list_remove(array_list, 0);    
+    entry = gonc_array_list_remove(array_list, 0);
+    assert_ptr_not_equal(entry, NULL);
     assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 200);
     gonc_entry_destroy(entry);
 
-    entry = gonc_array_list_remove(array_list, 0);    
+    entry = gonc_array_list_remove(array_list, 0);
+    assert_ptr_not_equal(entry, NULL);
     assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 100);
     gonc_entry_destroy(entry);
 
     gonc_array_list_destroy(array_list);
 }
 
-/*void append_and_remove_test()
+void append_and_remove_test()
 {
     struct gonc_array_list* array_list = gonc_array_list_create(5);
     assert_ptr_not_equal(array_list, NULL);
-    int p1 = 100;
-    assert_int_not_equal(gonc_array_list_append(array_list, &p1, sizeof(int)), -1);
-    p1 = 200;
-    assert_int_not_equal(gonc_array_list_append(array_list, &p1, sizeof(int)), -1);
-    p1 = 300;
-    assert_int_not_equal(gonc_array_list_append(array_list, &p1, sizeof(int)), -1);
-    p1 = 400;
-    assert_int_not_equal(gonc_array_list_append(array_list, &p1, sizeof(int)), -1);
-    p1 = 500;
-    assert_int_not_equal(gonc_array_list_append(array_list, &p1, sizeof(int)), -1);
-    int p2;
-    assert_int_not_equal(gonc_array_list_remove(array_list, 4, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 500);
-    assert_int_not_equal(gonc_array_list_remove(array_list, 3, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 400);
-    assert_int_not_equal(gonc_array_list_remove(array_list, 2, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 300);
-    assert_int_not_equal(gonc_array_list_remove(array_list, 1, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 200);
-    assert_int_not_equal(gonc_array_list_remove(array_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 100);
+
+    int* value;
+    value = malloc(sizeof(int));
+    *value = 100;
+    assert_int_not_equal(gonc_array_list_append(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 200;
+    assert_int_not_equal(gonc_array_list_append(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 300;
+    assert_int_not_equal(gonc_array_list_append(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 400;
+    assert_int_not_equal(gonc_array_list_append(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 500;
+    assert_int_not_equal(gonc_array_list_append(array_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    struct gonc_entry* entry = gonc_array_list_remove(array_list, 0);   
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 100);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_array_list_remove(array_list, 0);    
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 200);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_array_list_remove(array_list, 0);    
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 300);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_array_list_remove(array_list, 0);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 400);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_array_list_remove(array_list, 0);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 500);
+    gonc_entry_destroy(entry);
 
     gonc_array_list_destroy(array_list);
 }
-*/
+
 int main()
 {
     struct CMUnitTest tests[] = {
         cmocka_unit_test(general_test),
         cmocka_unit_test(prepend_and_remove_test),
-//        cmocka_unit_test(append_and_remove_test)
+        cmocka_unit_test(append_and_remove_test)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
