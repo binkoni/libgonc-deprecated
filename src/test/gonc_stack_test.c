d8179f15d92dc83d09ba794e59f221665400dade
Byeonggon Lee <gonny952@gmail.com>
2016-06-28 04:24:30 +0900

    gonc_stack passed the test

--- src/test/gonc_stack_test.c
+++ src/test/gonc_stack_test.c
@@ -3,38 +3,58 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include "../main/gonc_stack.h"
+#include "../main/gonc_entry.h"
+#include "../main/gonc_primitive.h"
 
 void test()
 {
     struct gonc_stack* stack = gonc_stack_create();
     assert_ptr_not_equal(stack, NULL);
 
-    int p1;
-    p1 = 100;
-    assert_int_not_equal(gonc_stack_push(stack, &p1, sizeof(int)), -1);
-    p1 = 200;
-    assert_int_not_equal(gonc_stack_push(stack, &p1, sizeof(int)), -1);
-    p1 = 300;
-    assert_int_not_equal(gonc_stack_push(stack, &p1, sizeof(int)), -1);
-    p1 = 400;
-    assert_int_not_equal(gonc_stack_push(stack, &p1, sizeof(int)), -1);
-    p1 = 500;
-    assert_int_not_equal(gonc_stack_push(stack, &p1, sizeof(int)), -1);
+    int* value = malloc(sizeof(int));
+    *value = 100;
+    assert_int_not_equal(gonc_stack_push(stack, gonc_primitive_create_entry(value, sizeof(int))), -1);
 
-    int p2;
-    assert_int_not_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 500);
-    assert_int_not_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 400);
-    gonc_stack_peek(stack, &p2, sizeof(int));
-    assert_int_equal(p2, 300);
-    assert_int_not_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 300);
-    assert_int_not_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 200);
-    assert_int_not_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 100);
-    assert_int_equal(gonc_stack_pop(stack, &p2, sizeof(int)), -1);
+    value = malloc(sizeof(int));
+    *value = 200;
+    assert_int_not_equal(gonc_stack_push(stack, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 300;
+    assert_int_not_equal(gonc_stack_push(stack, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 400;
+    assert_int_not_equal(gonc_stack_push(stack, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 500;
+    assert_int_not_equal(gonc_stack_push(stack, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    struct gonc_entry* entry = gonc_stack_pop(stack);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 500);
+    gonc_entry_destroy(entry);
+    
+    entry = gonc_stack_pop(stack);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 400);
+    gonc_entry_destroy(entry);
+    
+    entry = gonc_stack_pop(stack);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 300);
+    gonc_entry_destroy(entry);
+    
+    entry = gonc_stack_pop(stack);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 200);
+    gonc_entry_destroy(entry);
+    
+    entry = gonc_stack_pop(stack);
+    assert_ptr_not_equal(entry, NULL);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 100);
+    gonc_entry_destroy(entry);
 
     gonc_stack_destroy(stack);
 }
