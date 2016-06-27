acd374ce7f051b6833a7bedbe712a50326197d01
Byeonggon Lee <gonny952@gmail.com>
2016-06-27 09:34:27 +0900

    gonc_linked_list passed the test

--- src/test/gonc_linked_list_test.c
+++ src/test/gonc_linked_list_test.c
@@ -55,71 +55,113 @@ void general_test()
 
     gonc_linked_list_destroy(linked_list);
 }
-/*
+
 void prepend_and_remove_test()
 {
     struct gonc_linked_list* linked_list = gonc_linked_list_create();
     assert_ptr_not_equal(linked_list, NULL);
-    int p1 = 100;
-    assert_int_not_equal(gonc_linked_list_prepend(linked_list, &p1, sizeof(int)), -1);
-    p1 = 200;
-    assert_int_not_equal(gonc_linked_list_prepend(linked_list, &p1, sizeof(int)), -1);
-    p1 = 300;
-    assert_int_not_equal(gonc_linked_list_prepend(linked_list, &p1, sizeof(int)), -1);
-    p1 = 400;
-    assert_int_not_equal(gonc_linked_list_prepend(linked_list, &p1, sizeof(int)), -1);
-    p1 = 500;
-    assert_int_not_equal(gonc_linked_list_prepend(linked_list, &p1, sizeof(int)), -1);
-    int p2;
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 500);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 400);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 300);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 200);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 100);
+
+    int* value;
+
+    value = malloc(sizeof(int));
+    *value = 100;
+    assert_int_not_equal(gonc_linked_list_prepend(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 200;
+    assert_int_not_equal(gonc_linked_list_prepend(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 300;
+    assert_int_not_equal(gonc_linked_list_prepend(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 400;
+    assert_int_not_equal(gonc_linked_list_prepend(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 500;
+    assert_int_not_equal(gonc_linked_list_prepend(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    struct gonc_entry* entry = gonc_linked_list_remove(linked_list, 0);    
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 500);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 0);    
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 400);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 0);    
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 300);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 0);    
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 200);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 0);    
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 100);
+    gonc_entry_destroy(entry);
 
     gonc_linked_list_destroy(linked_list);
 }
+
 
 void append_and_remove_test()
 {
     struct gonc_linked_list* linked_list = gonc_linked_list_create();
     assert_ptr_not_equal(linked_list, NULL);
-    int p1 = 100;
-    assert_int_not_equal(gonc_linked_list_append(linked_list, &p1, sizeof(int)), -1);
-    p1 = 200;
-    assert_int_not_equal(gonc_linked_list_append(linked_list, &p1, sizeof(int)), -1);
-    p1 = 300;
-    assert_int_not_equal(gonc_linked_list_append(linked_list, &p1, sizeof(int)), -1);
-    p1 = 400;
-    assert_int_not_equal(gonc_linked_list_append(linked_list, &p1, sizeof(int)), -1);
-    p1 = 500;
-    assert_int_not_equal(gonc_linked_list_append(linked_list, &p1, sizeof(int)), -1);
-    int p2;
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 4, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 500);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 3, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 400);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 2, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 300);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 1, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 200);
-    assert_int_not_equal(gonc_linked_list_remove(linked_list, 0, &p2, sizeof(int)), -1);
-    assert_int_equal(p2, 100);
+
+    int* value = malloc(sizeof(int));
+    *value = 100;
+    assert_int_not_equal(gonc_linked_list_append(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 200;
+    assert_int_not_equal(gonc_linked_list_append(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 300;
+    assert_int_not_equal(gonc_linked_list_append(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 400;
+    assert_int_not_equal(gonc_linked_list_append(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+    value = malloc(sizeof(int));
+    *value = 500;
+    assert_int_not_equal(gonc_linked_list_append(linked_list, gonc_primitive_create_entry(value, sizeof(int))), -1);
+
+
+    struct gonc_entry* entry = gonc_linked_list_remove(linked_list, 4);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 500);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 3);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 400);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 2);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 300);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 1);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 200);
+    gonc_entry_destroy(entry);
+
+    entry = gonc_linked_list_remove(linked_list, 0);
+    assert_int_equal(*(int*)((struct gonc_primitive*)entry->data)->value, 100);
+    gonc_entry_destroy(entry);
 
     gonc_linked_list_destroy(linked_list);
 }
-*/
+
 int main()
 {
     struct CMUnitTest tests[] = {
         cmocka_unit_test(general_test),
-//        cmocka_unit_test(prepend_and_remove_test),
-//        cmocka_unit_test(append_and_remove_test)
+        cmocka_unit_test(prepend_and_remove_test),
+        cmocka_unit_test(append_and_remove_test)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
